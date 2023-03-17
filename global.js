24727d36a360a371b1e508c06034033c04ddf178
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-03-17 23:37:51 +0530

    Update global.js

--- global.js
+++ global.js
@@ -320,4 +320,15 @@ companies.forEach((company,index)=>{
     
 
 //filter
+const ages =[33,12,20,16,5,54,21,44,61,13,15,45,25,64,32];
 
+//by 1st method
+const age = ages.filter(function(age){
+    if(age>=20){
+        return true;
+    }
+})
+
+//by 2nd method
+const finalage = ages.filter(age => age >=30)
+console.log(finalage)
